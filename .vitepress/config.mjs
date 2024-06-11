847c28bda9fc6f9563deb7735e14494428cfcf16
劳资蜀道山 <1493170339@qq.com>
2024-06-11 14:18:34 +0800

    fix

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,9 +9,25 @@ export default defineConfig({
   base: "/docs/",
   
   themeConfig: {
-    logo: '/static/icons/logo.svg',
+    logo: '/docs/static/icons/logo.svg',
     search: {
-      provider: 'local'
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
     },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
