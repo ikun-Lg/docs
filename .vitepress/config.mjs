d4518fda5a58c9a197b2f74de3d4c62169750be9
劳资蜀道山 <1493170339@qq.com>
2024-06-11 14:11:10 +0800

    improve ui

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,29 +2,43 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lastUpdated: true,
+  head: [['link', { rel: 'icon', href: 'https://cn.vitejs.dev/logo.svg' }]],
   title: "头发不要落光",
   description: "个人学习文档 | 个人作品集",
-  base: "/docs/",
+  // base: "/docs/",
+  
   themeConfig: {
+    logo: '/static/icons/logo.svg',
+    search: {
+      provider: 'local'
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '个人作品集', link: '/works' },
-      { text: '学习文档', link: '/' },
+      { text: '个人作品集', link: '/docs/works' },
+      { text: '学习文档', link: '/docs/notes' },
       
     ],
 
-    sidebar: [
-      {
-        text: '我的作品',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    // sidebar: [
+    //   {
+    //     text: '我的作品',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/ikun-Lg' }
     ]
+  },
+
+  footer: {
+    message: 'Released under the MIT License.',
+    copyright: 'Copyright © 2024-present LG'
   }
 })
+
+
